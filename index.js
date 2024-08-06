0a2eb274e5c41731d05dd69f6be4a6c7a4de8e45
unknown <j.newm500@gmail.com>
2024-08-06 13:27:13 -0500

    added about page changed some content

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ app.get("/", (req, res) => {
     res.render("index.ejs");
 });
 
+app.get("/create", (req, res) => {
+    res.render("create.ejs");
+});
+
+app.get("/about", (req, res) => {
+    res.render("about.ejs");
+});
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
